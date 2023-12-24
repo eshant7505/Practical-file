a16f29bff1e9bab4495a291f707c382c26b4f12c
eshant7505 <153259930+eshant7505@users.noreply.github.com>
2023-12-24 23:00:22 +0530

    Add files via upload

---
+++ linear_search_in_array.c
@@ -0,0 +1,38 @@
+//linear search in array
+#include <stdio.h>
+
+int linearSearch(int arr[], int n, int key) {
+    for (int i = 0; i < n; i++) {
+        if (arr[i] == key) {
+            return i; // Return the index if the element is found
+        }
+    }
+    return -1; // Return -1 if the element is not found
+}
+
+int main() {
+    int n, key;
+
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+
+    printf("Enter elements of the array:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter the element to search: ");
+    scanf("%d", &key);
+
+    int result = linearSearch(arr, n, key);
+
+    if (result != -1) {
+        printf("Element %d found at index %d.\n", key, result);
+    } else {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
