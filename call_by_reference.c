4adb9aa66a68ea2a3c9132a832a7b6e639dddb1e
eshant7505 <153259930+eshant7505@users.noreply.github.com>
2025-08-05 13:02:25 +0530

    Update call_by_reference.c

--- call_by_reference.c
+++ call_by_reference.c
@@ -1,7 +1,6 @@
 //call by reference using pointers
 #include <stdio.h>
 
-// Function to swap two integers using pointers
 void swap(int *a, int *b) {
     int temp = *a;
     *a = *b;
@@ -10,20 +9,15 @@ void swap(int *a, int *b) {
 
 int main() {
     int num1, num2;
-
-    // Input two numbers from the user
     printf("Enter the first number: ");
     scanf("%d", &num1);
     printf("Enter the second number: ");
     scanf("%d", &num2);
 
-    // Display the numbers before swapping
     printf("Before swapping: num1 = %d, num2 = %d\n", num1, num2);
 
-    // Call the swap function with pointers to num1 and num2
     swap(&num1, &num2);
 
-    // Display the numbers after swapping
     printf("After swapping: num1 = %d, num2 = %d\n", num1, num2);
 
     return 0;
