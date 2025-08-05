7ab9a8959d34127acb1ce0cf75ba1d6c6ab8f0bc
eshant7505 <153259930+eshant7505@users.noreply.github.com>
2025-08-05 13:05:16 +0530

    Update file_handling_C.c

--- file_handling_C.c
+++ file_handling_C.c
@@ -5,7 +5,6 @@ int main() {
     FILE *file;
     char data[100];
 
-    // Open the file in append mode ("a")
     file = fopen("example.txt", "a");
 
     if (file == NULL) {
@@ -13,14 +12,11 @@ int main() {
         return 1; // Return an error code
     }
 
-    // Get data from the user
     printf("Enter data to append to the file: ");
     fgets(data, sizeof(data), stdin);
 
-    // Append data to the file
     fprintf(file, "%s", data);
 
-    // Close the file
     fclose(file);
 
     printf("Data appended to the file successfully.\n");
